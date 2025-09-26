24ecc539ff975372382391e62940c8b744ae875b
FireballKitty <fireballkittydev@gmail.com>
2025-09-26 23:44:07 +0200

    Refactor tile range initialization for better readability

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from context import Context
 from vectors import Vec2, VecRange
 
+# ======== Configuration ========
+
+start = Vec2(100, 100)
+end = Vec2(104, 104)
+
+# ===============================
+
 if __name__ == "__main__":
     context = Context(
-        tile_range = VecRange(Vec2(100,100), Vec2(104,104)))
+        tile_range = VecRange(start, end))
     context.fetch_all_tiles()
     context.save_all_tiles(with_timestamp=True, format="png")
